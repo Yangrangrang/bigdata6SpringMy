e586315efd3410c362fa0ef29a8ab9ae3cda0d12
yangrangrang <gkssk2309@gmail.com>
2022-12-14 12:02:35 +0900

    first commit

---
+++ src/main/java/com/example/bigdata6springstudy/BigData6SpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.bigdata6springstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigData6SpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigData6SpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/bigdata6springstudy/BigData6SpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bigdata6springstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigData6SpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
